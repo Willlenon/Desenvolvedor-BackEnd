d9bb91f97a5a4fbb522b1c23fb13ed07472b199c
willlenon <lenon78@gmail.com>
2026-07-17 20:36:38 -0300

    atividade 01 professor cesar

---
+++ Aula01/BackEndSENAI/BackEndSENAI/Program.cs
@@ -0,0 +1,13 @@
+﻿double x, y, media;
+Console.WriteLine("Digite o primeiro número: ");
+
+x = double.Parse(Console.ReadLine());
+
+Console.WriteLine(" Digite umnumero");
+
+y = double.Parse(Console.ReadLine());
+
+media = (x + y) / 2.0;
+
+Console.WriteLine("A média é: " + media);
+
