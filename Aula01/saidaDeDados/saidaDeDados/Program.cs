35977428db21d46b8badbb1c31f1bf1abd827c28
willlenon <lenon78@gmail.com>
2026-07-17 21:51:29 -0300

    Finalizado aula 1

---
+++ Aula01/saidaDeDados/saidaDeDados/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;// using static System.Console; permite que você use os métodos da classe Console sem precisar digitar Console. antes de cada método
+
+
+int numero = 12;
+
+double flutuante = 2.85;
+
+string palavra = "Senai";
+
+char caractere = 'S';
+
+bool logica1 = true;
+
+bool logica2 = false;
+
+
+WriteLine("Hello, World!");
+WriteLine($" {numero} , {flutuante} ,{caractere},{logica1}");
+WriteLine("saida de dados quando exibe informações para o uduario ");// writeline quebra linha 
+WriteLine("Senai Americana ");
+WriteLine("Curso DEV BackEnd ");
+Write("Espanha ");// write não quebra linha
+ReadKey();// ReadKey() espera o usuário pressionar uma tecla para encerrar o programa
