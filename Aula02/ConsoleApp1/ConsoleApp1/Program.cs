a399a5c23428824b3de88617ffbfeb3f2939c48c
willlenon <lenon78@gmail.com>
2026-07-20 21:53:23 -0300

    declaraçao de variaveis entrada processamento e saida de dados

---
+++ Aula02/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+
+double largura = 0;
+double comprimento = 0;
+double valor = 1500;
+double area = 0;
+double preco = 0;
+string nome;
+double telefone;
+string email;
+
+Console.WriteLine("Digite o nome do cliente:"); 
+nome = Console.ReadLine();
+
+Console.WriteLine("Digite o email: ");
+email = Console.ReadLine();
+
+Console.WriteLine("Digite o telefone: ");
+telefone = double.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine("Digite a largura do terreno em metros:");
+largura = double.Parse(Console.ReadLine());
+
+Console.WriteLine("""Digite o comprimento do terreno em metros:""");
+comprimento = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor do metro quadrado:");
+valor = double.Parse(Console.ReadLine());
+
+area = largura * comprimento;
+
+preco = area * valor;
+
+Console.WriteLine($"""
+    ==================================================================
+    =                                                                
+    = Cliente : {nome}, Telefone :{telefone}, Email :{email}         
+    =                                                                
+    =,Area do imovel  é :{area}, o valor toltal é {preco}            
+    =                                                                
+    ==================================================================
+    """);
+
---
+++ Aula02/ConsoleApp1/ExercicioRetangulo/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+/*
+ * area base  = base * altura
+ * perimetro = base + altura + base + altura    
+ * diagonal = 
+ */
+double bAse;
+double altura;
+double area;
+double perimetro;
+double diagonal;
+
+Console.WriteLine("Digite a base do retangulo :  ");
+bAse = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a altura do retangulo :");
+altura = double.Parse(Console.ReadLine());
+
+
+area = bAse * altura;
+
+perimetro = bAse + altura + bAse + altura;
+
+diagonal = Math.Sqrt(Math.Pow(bAse, 2) + Math.Pow(altura, 2));
+
+Console.WriteLine($" a Area do retangulo é : {area.ToString("f4")}, o perimetro é : {perimetro:f4}, A diagonal é :{diagonal}");
+       
+
+
